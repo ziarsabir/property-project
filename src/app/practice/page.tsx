6d0e052d6e0e1b7f191b86a01065d106f80df141
Ziar Sabir <ziar.sabir@gmail.com>
2026-01-28 12:50:47 +0000

    Add practice page to test SavePropertyButton

---
+++ src/app/practice/page.tsx
@@ -0,0 +1,10 @@
+import SavePropertyButton from "@/components/SavePropertyButton";
+
+export default function PracticePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Practice</h1>
+      <SavePropertyButton listingId="abc123" />
+    </main>
+  );
+}
